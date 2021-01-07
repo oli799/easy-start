3095054838f22e086ed331861b6ba2490bedbbfb
oli799 <reider340@gmail.com>
2021-01-07 22:54:16 +0100

    connect main thread with renderer, create folder, create preset

--- js/index.js
+++ js/index.js
@@ -1,11 +1,34 @@
+const { ipcRenderer } = require('electron');
 const { dialog } = require('electron').remote;
 
+const cerateProjectForm = document.querySelector('#createProjectFrom');
 const workingDirButton = document.querySelector('#workingDirButton');
 const workongDirInput = document.querySelector('#workingDirInput');
 const createProjectButton = document.querySelector('#createProjectButton');
 const openProjectButton = document.querySelector('#openProjectButton');
 
+cerateProjectForm.addEventListener('submit', createProject);
 workingDirButton.addEventListener('click', getWorkingDir);
+
+function createProject(e) {
+  e.preventDefault();
+  const requestObject = {
+    with_git: '',
+  };
+  const formData = new FormData(cerateProjectForm);
+
+  for (let inputField of formData.entries()) {
+    requestObject[inputField[0]] = inputField[1];
+  }
+
+  // call main thread function
+  ipcRenderer.send('request-create-new-project', requestObject);
+
+  // listen response from main thread
+  ipcRenderer.on('response-create-new-project', function (event, arg) {
+    console.log(arg);
+  });
+}
 
 async function getWorkingDir() {
   const result = await dialog.showOpenDialog({ properties: ['openDirectory'] });
--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-const { app, BrowserWindow, dialog } = require('electron');
+const { execSync } = require('child_process');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const fs = require('fs');
 
 // reloading app after every change
 require('electron-reload')(__dirname);
@@ -29,3 +31,82 @@ app.on('activate', function () {
     createWindow();
   }
 });
+
+// listen calls from index.js
+ipcMain.on('request-create-new-project', async function (event, arg) {
+  const projectFolderPath = `${arg.working_dir}/${arg.project_name}`;
+
+  // create project folder
+  const isFolderCreated = createProjectFolder(projectFolderPath);
+
+  // check if folder  created
+  if (!isFolderCreated) {
+    return event.reply(
+      'response-create-new-project',
+      'This folder is already exists in your working directory!'
+    );
+  }
+
+  // get and save selected preset
+  const isPresetCreated = await createSelectedPreset(
+    arg.preset_type,
+    projectFolderPath
+  );
+
+  // check if presert created
+  if (!isPresetCreated) {
+    // TODO: if preset create fails, delete project directory
+
+    return event.reply(
+      'response-create-new-project',
+      'Error when creating your preset!'
+    );
+  }
+
+  if (!!arg.with_git) {
+    // create git repo
+  }
+
+  return event.reply('response-create-new-project', 'Done!');
+});
+
+async function createSelectedPreset(type, path) {
+  switch (type) {
+    case 'npm': {
+      return execSync('npm init -y', { cwd: path });
+    }
+    case 'django': {
+      try {
+        return execSync('git cone https://github.com/wsvincent/djangox.git .', {
+          cwd: path,
+        });
+      } catch (error) {
+        return 0;
+      }
+    }
+    case 'express': {
+      try {
+        return execSync(
+          'git cone https://github.com/latifs/simple-express.git .',
+          {
+            cwd: path,
+          }
+        );
+      } catch (error) {
+        return 0;
+      }
+    }
+    case 'empty': {
+      break;
+    }
+  }
+}
+
+function createProjectFolder(path) {
+  if (!fs.existsSync(path)) {
+    fs.mkdirSync(path);
+    return path;
+  }
+
+  return false;
+}
