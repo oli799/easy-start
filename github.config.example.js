afede3c3d7d84c39d188cc7173ac06076b871044
oli799 <reider340@gmail.com>
2021-01-29 18:34:51 +0100

    update config.example.js

--- github.config.example.js
+++ github.config.example.js
@@ -1,7 +1,7 @@
 const config = {
   client_id: 'github_oauth_client_id',
   client_secret: 'github_oauth_client_secret',
-  scopes: ['repo'],
+  scopes: ['repo', 'user'],
 };
 
 module.exports = config;
